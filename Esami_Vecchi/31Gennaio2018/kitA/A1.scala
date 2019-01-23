423fae8274da7729273a6975b21f5f6d8c849b85
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2019-01-23 10:34:03 +0100

    soluzioni esercizi 1,2 esame 31/1/18 A

--- Esami_Vecchi/31Gennaio2018/kitA/A1.scala
+++ Esami_Vecchi/31Gennaio2018/kitA/A1.scala
@@ -1,3 +1,5 @@
 object A1 {
-	
+	def select(cars:List[Car]) = {
+		cars.groupBy(c=>c.year).toList.map(p=>(p._1, p._2.map(_.owner).reduce((a,b)=>if(a.age<b.age) a else b).name))
+	}
 }
--- Esami_Vecchi/31Gennaio2018/kitA/A2.scala
+++ Esami_Vecchi/31Gennaio2018/kitA/A2.scala
@@ -1,1 +1,10 @@
-// scrivere soluzione qui...
+object A2 {
+	def mywhile(test: => Boolean, s: => Any)(body: => Unit):Unit = {
+		if (!test) ()
+		else {
+			println(s)
+			body
+			mywhile(test, s)(body)
+		}
+	}
+}
