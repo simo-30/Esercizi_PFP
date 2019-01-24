6d8379e67807c9a3356877bc50e4c10408f54cba
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2019-01-24 14:14:56 +0100

    sol B3

--- Esami_Vecchi/14Febbraio2017/kitB/B3/B3.scala
+++ Esami_Vecchi/14Febbraio2017/kitB/B3/B3.scala
@@ -1,1 +1,9 @@
-// scrivere la soluzione qui...
+object B3 {
+	def max(f1:Int=>String, f2:Int=>String):Int=>String = {
+		x => {
+			val y1=f1(x)
+			val y2=f2(x)
+			if (y1.length>y2.length) y1 else y2
+		}
+	}
+}
